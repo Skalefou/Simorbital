5e57b9caa97f7c06b912ef8d38d273300179b713
Skalefou <skamsagar@gmail.com>
2022-12-09 22:23:26 +0100

    Display of the stars

--- src/celestialBody.py
+++ src/celestialBody.py
@@ -1,3 +1,22 @@
+import pygame
+from math import ceil
+from constante import *
+
 class CelestialBody:
-    def __init__(self, distance):
+    def __init__(self, name, distance, radius, color):
+        self.name = name
         self.distance = distance
+        self.radius = radius
+        self.color = color
+        
+        if self.radius >= 100000:
+            self.radiusAbs = ceil((self.radius/8)/800)
+        elif self.radius >= 1000:
+            self.radiusAbs = ceil(self.radius/500)
+        if self.distance != 0:
+            self.position = [SCREEN_WIDTH/2 + 300, SCREEN_HEIGHT/2]
+        else:
+            self.position = [SCREEN_WIDTH/2, SCREEN_HEIGHT/2]
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.position, self.radiusAbs)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
 import pygame
 from celestialBody import CelestialBody
+from constante import *
+
+#TODO: Add text to be displayed over the celestials bodys
 
 class Program:
     def __init__(self):
-        self.sun = CelestialBody(0)
+        self.celestialBody = [CelestialBody("Sun", 0, SUN_RADIUS, COLOR_SUN), CelestialBody("Earth", DISTANCE_SUN_EARTH, EARTH_RADIUS, COLOR_EARTH)]
+        self.screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+
+        pygame.init()
+        pygame.display.set_caption("Simorbital")
+    
+    def run(self):
+        running = True
+        while running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            self.screen.fill((0, 0, 0))
+            
+            i = 0
+            for i in range(len(self.celestialBody)):
+                self.celestialBody[i].draw(self.screen)
+            
+            pygame.display.flip()
+
+    def __del__(self):
+        pygame.quit()
+        
 
 program = Program()
+program.run()
